ef66ad47ae9a9f41aa75511dd144811b52aa6acf
Ali Imron <ali@kanosolution.com>
2020-07-03 15:01:13 +0700

    add breaking point browser dev

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="breaking">Try to Breakpoint</button>
   </div>
 </template>
+
+<script>
+export default {
+  name: "about-page",
+  methods: {
+    breaking() {
+      let i = 0;
+      for (i; i < 10; i++) {
+        console.log(`loop you ${i}`);
+      }
+      console.log("last i", i);
+      alert("breaking");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
